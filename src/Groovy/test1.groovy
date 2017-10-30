2a50bc3071256c46efe0a926559638fa86777ac5
wanderzzs <zzsfrankzhang@gmail.com>
2017-10-30 12:28:57 -0400

    First

---
+++ src/Groovy/test1.groovy
@@ -0,0 +1,13 @@
+println "Welcome to Groovy"
+println "Today is" + Date().toString()
+
+def sum = 3+5
+printlin "the sum is" + sum
+
+System.out.println("Hello Test");
+
+def beatles = ["John", "Paul", "Gerogre", "Ringo"]
+for (int i =0; i< beatle.size(); i++){
+    def greeting = "Hello"    //$ is the G string
+    println $greeting + beatles[i]
+}
