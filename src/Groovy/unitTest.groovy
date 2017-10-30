a3797a793b9c0e4be5280486f4c9a796d84fce84
wanderzzs <zzsfrankzhang@gmail.com>
2017-10-30 15:37:36 -0400

    Unit

---
+++ src/Groovy/unitTest.groovy
@@ -0,0 +1,17 @@
+//Fast Isolated Repeatable self-verifying timely
+//TDD Test-driven Development
+//1.clean API 2.Testable Code is Flexable 3.Results in much less production code
+//4. no need to schedule 'unit testing' time
+
+class BankAccount{
+    private balance
+
+    BankAccount(openningBalance){
+        balance = openningBalance
+    }
+
+    def deposite(amount){
+        balance += amount
+    }
+}
+
